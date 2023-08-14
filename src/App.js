b8eccaa37c74ac34cfc39ca4a4c44adc21bc2e0d
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-14 12:51:12 +0300

    added account route, removed user passing as props

--- src/App.js
+++ src/App.js
@@ -8,40 +8,31 @@ import CardPageWrap from './components/views/CardPage/CardPageWrap';
 import PaymentsInfo from './components/views/Information/PaymentsInfoPage/PaymentsInfoPage';
 import ShoppingCartPage from './components/views/ShoppingCartPage/ShoppingCartPage';
 import ContactsInfo from './components/views/Information/ContactsInfoPage/ContactsPage';
-import LoginPage from './components/views/Account/LoginPage/LoginPage';
-import SignUpPage from './components/views/Account/SignUpPage/SignUpPage';
-
+import LoginPage from './components/views/User/LoginPage/LoginPage';
+import SignUpPage from './components/views/User/SignUpPage/SignUpPage';
+import AccountPage from 'components/views/User/AccountPage/AccountPage.js';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {user: {id: 0}}
-  
-    this.setUser = this.setUser.bind(this);
   };
-
-  setUser(res){
-    this.state.user = res;
-    this.forceUpdate()
-    console.log('user data has changed');
-    console.log(this.state);
-  }
 
   render(){
     return (
       <div>
         <BrowserRouter>
-            <Menu user={this.state.user}/>
+            <Menu/>
             <Routes>   
               <Route path="/" element={<Navigate replace to="/catalog" />} />
-              <Route path="/catalog" element={<CatalogPage user={this.state.user}/>} />
-              <Route path="/product" element={<CardPageWrap user={this.state.user}/>} />
+              <Route path="/catalog" element={<CatalogPage/>} />
+              <Route path="/product" element={<CardPageWrap/>} />
               <Route path="/payments" element={<PaymentsInfo />} /> 
               <Route path="/contacts" element={<ContactsInfo />} /> 
-              <Route path="/account" element={<LoginPage setUser={this.setUser} user={this.state.user}/>} />    
+              <Route path="/login" element={<LoginPage/>} />    
               <Route path="/signup" element={<SignUpPage/>} />
-              <Route path="/cart" element={<ShoppingCartPage user={this.state.user}/>} />    
+              <Route path="/account" element={<AccountPage/>} />
+              <Route path="/cart" element={<ShoppingCartPage/>} />    
             </Routes>
           </BrowserRouter>
       </div> 
