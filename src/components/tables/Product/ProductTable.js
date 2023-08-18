ee6bd51b0be8eb82891874887bbdc5bb571c5e44
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-18 17:38:21 +0300

    Create ProductTable.js

---
+++ src/components/tables/Product/ProductTable.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+import Product from 'components/views/Product/Product';
+
+import './ProductTable.styles.css'
+
+import store from "store";
+import api from 'api';  
+
+class ProductTable extends React.Component {
+
+    constructor(props) {
+        super(props);
+    };
+
+    updateTable()
+    {
+        this.forceUpdate();
+    }
+
+    render()
+    {
+        if (this.props.mode == "cart")
+        {
+            return(
+                <table className="products-table">
+                    <tbody>
+                        <tr>
+                            <td className="products-table-type-cell"><p>Type</p></td>
+                            <td className="products-table-cell"><p>Width</p></td>
+                            <td className="products-table-cell"><p>Height</p></td>
+                            <td className="products-table-cell"><p>Length</p></td>
+                            <td className="products-table-cell"><p>Price</p></td>
+                            <td className="products-table-cell"><p>Count</p></td>
+                        </tr> 
+                        {this.props.products.map((element) => (
+                            <Product key={element.id} json={element} mode={this.props.mode} updateTable={this.updateTable}></Product>
+                        ))}
+                    </tbody>
+                </table>
+            )
+        }    
+        else if (this.props.mode == "cart")
+        {
+            return(
+                <table className="products-table">
+                    <tbody>
+                        <tr>
+                            <td className="products-table-cell"><p>Width</p></td>
+                            <td className="products-table-cell"><p>Height</p></td>
+                            <td className="products-table-cell"><p>Length</p></td>
+                            <td className="products-table-cell"><p>Price</p></td>
+                            <td className="products-table-cell"><p>In stock</p></td>
+                        </tr>
+                        {this.state.posts.products.map(element => (
+                            <Product key={element.id} json={element} mode={this.props.mode} updateTable={this.updateTable}></Product>
+                        ))} 
+                    </tbody>
+                </table>
+            )
+        }
+    }
+}
+
+export default ProductTable;
