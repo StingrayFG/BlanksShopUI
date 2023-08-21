a209fb65bb6d069e8a85d4ae2d2b5bccacedd5d8
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-21 16:11:43 +0300

    added orders fetch

--- src/components/views/User/AccountPage/AccountPage.js
+++ src/components/views/User/AccountPage/AccountPage.js
@@ -1,14 +1,16 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { get, set, useForm } from 'react-hook-form';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, userSelector, clearState, clearUser } from 'services/User/UserSlice';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import OrderTable from 'components/tables/Order/OrderTable'
 
 import '../LoginPage.styles.css';
 
 import store from "store";
+import api from 'api';
 
 const AccountPage = ({}) => {
 
@@ -26,29 +28,29 @@ const AccountPage = ({}) => {
     navigate('/')
   };
 
-  useEffect(() => {
-    return () => {
-      dispatch(clearState());
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isError) {
-      toast.error(errorMessage);
-      dispatch(clearState());
-    }
-
-    if (isSuccess) {
-      dispatch(clearState());
-      navigate('/');
-    }
-  }, [isError, isSuccess]);
+  async function getOrders () {
+    await fetch(api.baseUrl + 'order/get/bycustomer?customerID=' + store.getState().user.id)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        setOrdersList(res);
+      })
+      .catch(error => console.error(error))
+  }
   
+  const [ordersList, setOrdersList] = useState([])
+
+  useEffect(() => {
+    getOrders()
+  }, [])
+
   return(
       <div className="std">
         <h2>Account</h2>
         <h2>{store.getState().user.name}</h2>
         <button onClick={logOut}><p>Log Out</p></button>
+        <h2>Orders</h2>
+        <OrderTable orders={ordersList}></OrderTable>
       </div>
   ) 
 }
